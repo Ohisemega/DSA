8f9723adf17ab76aa00bc8c1555b6e311496f2ca
Alele Famous <famousalele@gmail.com>
2021-09-28 13:07:19 +0100

    optimized memory by reducing the use of vectors. removed bug from stack pile creation

--- equilibrum_matrix.cpp
+++ equilibrum_matrix.cpp
@@ -5,11 +5,11 @@
 int solution(std::vector< std::vector<int> > &a){
 	const int rowNum = a.size();
 	const int columnNum = a[0].size();
-	std::vector<int> rowSums(rowNum);
+	int rowSums[rowNum];
 	std::vector<int> columnSums(columnNum);
-	std::vector<int> equilibrumRows((rowNum/2) + 1);
-	std::vector<int> equilibrumColumns((columnNum/2) + 1);
-	std::stack<int>stack;
+	int equRows = 0;
+	int equColumns = 0;
+	std::stack<int>stackSum;
 
 	for(int i = 0; i < rowNum; ++i){
 		for(int j = 0; j < columnNum; ++j){
@@ -25,36 +25,38 @@ int solution(std::vector< std::vector<int> > &a){
 
 	//create the stack for rows sum aggregate
 	int sum = 0;
-	stack.push(sum);
-	for(int i = rowNum - 2; i >= 0; --i){
+	stackSum.push(sum);
+	for(int i = 0; i <= rowNum - 2; ++i){
 		sum += rowSums[i];
-		stack.push(sum);
+		stackSum.push(sum);
 	}
 
 	//compare the stack top and sum value to determine equilibrum rows
 	sum = 0;
 	for(int i = rowNum - 1; i >= 0; --i, sum += rowSums[i]){
-		if(stack.top() == sum){
-			equilibrumRows.push_back(i);
+		if(stackSum.top() == sum){
+			++equRows;
 		}
-		stack.pop();
+		stackSum.pop();
 	}
 
 	//create stack for columns aggregate
 	sum = 0;
-	stack.push(sum);
-	for(int i = columnNum - 2; i >= 0; --i){
+	stackSum.push(sum);
+	for(int i = 0; i <= columnNum - 2; ++i){
 		sum += columnSums[i];
-		stack.push(sum);
+		stackSum.push(sum);
 	}
 
 	//compare the stack top and sum value to determine equilibrum columns
 	sum = 0;
 	for(int i = columnNum - 1; i >= 0; --i, sum += columnSums[i]){
-		if(stack.top() == sum){
-			equilibrumColumns.push_back(i);
+		if(stackSum.top() == sum){
+			++equColumns;
 		}
-		stack.pop();
+		stackSum.pop();
 	}
-	return equilibrumRows.size() * equilibrumColumns.size();
+
+	//return the number of equilibrum points
+	return equRows * equColumns;
 }
