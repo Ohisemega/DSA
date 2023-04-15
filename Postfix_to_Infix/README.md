bacd790fd3a6127ff91e94d65f2f433b7dc111ec
Alele Famous <famousalele@gmail.com>
2023-04-15 13:56:04 +0100

    Postfix to Infix
    Postfix to Infix algorithm

---
+++ Postfix_to_Infix/README.md
@@ -0,0 +1,4 @@
+# Postfix to Infix.
+Computational Algorithm to convert an expression string from postfix notation to infix.
+<br> NB: '$' - sign represents the squareroot symbol
+
---
+++ Postfix_to_Infix/postfix_to_infix.cpp
@@ -0,0 +1,58 @@
+#include <string>
+#include <stack>
+#include <iostream>
+
+std::string postfixToInfix(std::string& postfixExp){
+	int len = postfixExp.length();
+	std::string output = "";
+	std::stack<std::string> stk;
+	std::string sum;
+	for(int i = 0; i < len; ++i){
+		if(postfixExp[i] == '-' && isdigit(postfixExp[i+1])){
+			++i;
+			std::string negVal = std::string() + "(" + "-" + postfixExp[i] + ")";
+			stk.push(negVal);
+		}else if(std::isdigit(postfixExp[i])){
+			stk.push(std::string() + postfixExp[i]);
+		}else if(postfixExp[i] == '+'){
+			sum = stk.top();
+			stk.pop();
+			sum = std::string() + "(" + stk.top() + "+" + sum + ")";
+			stk.pop();
+			stk.push(sum);
+		}else if(postfixExp[i] == '-'){
+			sum = stk.top();
+			stk.pop();
+			sum = std::string() + "(" + stk.top() + "-" + sum + ")";
+			stk.pop();
+			stk.push(sum);
+		}else if(postfixExp[i] == '*'){
+			sum = stk.top();
+			stk.pop();
+			sum = std::string() + "(" + stk.top() + "*" + sum + ")";
+			stk.pop();
+			stk.push(sum);
+		}else if(postfixExp[i] == '$'){
+			sum = stk.top();
+			stk.pop();
+			sum = std::string() + "$(" + stk.top() + ")";
+			stk.push(sum);
+		}else if(postfixExp[i] == '/'){
+			std::string denominator = stk.top();
+			stk.pop();
+			std::string numerator = stk.top();
+			stk.pop();
+			sum = std::string() + "(" + numerator + "/" + denominator + ")";
+			stk.push(sum);
+		}
+		std::cout << "The final value is: " << i << std::endl;
+		std::cout << "The stack top is: " << stk.top() << std::endl;
+	}
+	return stk.top();
+}
+
+int main(){
+	// std::string expr{"5 9 8 + 4 6 * * 7 + *"};
+	std::string expr{"-2 5 9 8 + 4 6 * * 7 $ + * -"};
+	std::cout << "The value is: " << postfixToInfix(expr);
+}
