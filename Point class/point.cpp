52377f9fe1e13ac4401114b4e90c3200b17b5578
Ohisemega <famousalele@gmail.com>
2023-10-05 15:37:47 +0100

    Implement an overloaded template function
    To check for equality of point objects.

--- Point class/point.cpp
+++ Point class/point.cpp
@@ -1,4 +1,5 @@
 #include "point.hpp"
+#include <iostream>
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
@@ -6,14 +7,23 @@
 template <typename T>
 Point<T>::Point(){
     std::srand(std::time(0));
-    this->_x = std::rand()/RAND_MAX * 20000;
-    this->_y = std::rand()/RAND_MAX * 20000;
+    this->_x = std::rand()/RAND_MAX * 2000000000.00;
+    this->_y = std::rand()/RAND_MAX * 2000000000.00;
 }
 
 template <typename T>
-Point<T>::Point(T x, T y){
-    this->_x{x};
-    this->_y{y};
+Point<T>::Point(T x, T y) : _x{x}, _y{y}{
+
+}
+
+template <typename T>
+T Point<T>::getX() const{
+    return this->_x;
+}
+
+template <typename T>
+T Point<T>::getY() const{
+    return this->_y;
 }
 
 template <typename T>
@@ -24,4 +34,15 @@ T Point<T>::distanceToOrigin() const{
 template <typename T>
 T Point<T>::distanceFromPoint(Point& next) const{
     return std::sqrt(std::pow(this->_x - next._x, 2) + std::pow(this->y - next._y, 2));
+}
+
+template <typename T>
+bool Point<T>::operator==(Point& p2) const{
+    return (this->distanceFromPoint(p2) < 0.000001);
+}
+template <typename T>
+std::ostream& operator<<(std::ostream& output, const Point<T> &p) {
+    output << "x: " << p._x << "y: " << p._y << "\n";
+
+    return output;
 }
--- Point class/point.hpp
+++ Point class/point.hpp
@@ -1,16 +1,23 @@
 #ifndef POINT_HPP
 #define POINT_HPP
 
+#include <string>
 template <typename dataT>
 class Point{
+    template <typename T>
+    friend std::ostream& operator<<(std::ostream& out, const Point<T>& p);
     public:
         Point();
         Point(dataT x_, dataT y_);
+        dataT getX() const;
+        dataT getY() const;
         dataT distanceToOrigin() const;
         dataT distanceFromPoint(Point& p2) const;
+        bool operator==(Point& p2) const;
 
     private:
         dataT _x, _y;
 };
+
 
 #endif
