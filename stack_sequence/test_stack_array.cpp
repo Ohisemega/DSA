88fe93c0151a736b95a2f864b900dd96d8ffd291
Ohisemega <famousalele@gmail.com>
2024-05-08 14:15:42 +0100

    Test Application for Stack

---
+++ stack_sequence/test_stack_array.cpp
@@ -0,0 +1,29 @@
+#include "stack_array.hpp"
+
+int main(){
+    STACK<int> myIntStack{STACK<int>(20)};
+    STACK<int> testCopy{5};
+    myIntStack.push(2);
+    myIntStack.printIntArray();
+    myIntStack.push(4);
+    myIntStack.printIntArray();
+    myIntStack.push(6);
+    myIntStack.printIntArray();
+    myIntStack.push(8);
+    myIntStack.printIntArray();
+    myIntStack.push(10);
+    myIntStack.printIntArray();
+    myIntStack.push(1);
+    myIntStack.printIntArray();
+    myIntStack.push(3);
+    myIntStack.printIntArray();
+    myIntStack.push(5);
+    myIntStack.printIntArray();
+    myIntStack.pop();
+    myIntStack.printIntArray();
+    myIntStack.push(2);
+    myIntStack.printIntArray();
+
+    testCopy = myIntStack;
+    testCopy.printIntArray();
+}
