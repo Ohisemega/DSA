92407931ae646da5a09f07217ab7d143d0770199
Ohisemega <famousalele@gmail.com>
2024-08-17 11:45:14 +0100

    Implement Sorting Algorithms
    Implement quick_sort
    Implement Randomized quick_sort

--- sorting_alg/randomized_quick_sort.cpp
+++ sorting_alg/randomized_quick_sort.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <random>
 
 template <typename type_t>
 int Partition(std::vector<type_t>& vec, int beg, int end){
@@ -16,19 +17,28 @@ int Partition(std::vector<type_t>& vec, int beg, int end){
     return i+1;
 }
 
+template <typename type_t>
+int randomized_partition(std::vector<type_t>& vec, int beg, int end){
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist(beg, end);
+
+    std::swap(vec[end], vec[dist(rng)]);
+    return Partition(vec, beg, end);
+}
 
 template <typename type_t>
-void QuickSort(std::vector<type_t>& vec, int start, int end ){
+void Randomized_QuickSort(std::vector<type_t>& vec, int start, int end ){
     if(start < end){
-        int pivot = Partition(vec, start, end);
-        QuickSort(vec, start, pivot-1);
-        QuickSort(vec, pivot + 1, end);
+        int pivot = randomized_partition(vec, start, end);
+        Randomized_QuickSort(vec, start, pivot-1);
+        Randomized_QuickSort(vec, pivot + 1, end);
     }
 }
 
 int main(){
     std::vector<int> vec{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    QuickSort(vec, 0, vec.size()-1);
+    Randomized_QuickSort(vec, 0, vec.size()-1);
 
     std::cout << "The array is: \n";
     for(auto itr: vec)
