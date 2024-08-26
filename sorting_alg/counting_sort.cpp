4fe4c5c009d4dc1bec4ce923882f6441c8a9493f
Ohisemega <famousalele@gmail.com>
2024-08-27 00:35:06 +0100

    Fix bugs with counting sort.

--- sorting_alg/counting_sort.cpp
+++ sorting_alg/counting_sort.cpp
@@ -3,24 +3,39 @@
 
 template <typename type_t>
 std::vector<type_t> counting_sort(std::vector<type_t> input, u_int32_t maxElem){
-    std::vector freq(maxElem + 1, 0);
     std::vector freqAcc(maxElem + 1, 0);
 
     for(auto& elem : input){
-        ++freq[elem];
+        ++freqAcc[elem];
     }
 
-    freqAcc[0] = freq[0];
-    for(auto elem = 0; elem < maxElem + 1; ++freq){
-        freqAcc[elem] = freq[elem] + freqAcc[elem - 1];
+    for(auto elem : freqAcc)
+        std:: cout << elem << " ";
+    std::cout << "\n";
+
+    for(auto elem = 1; elem < freqAcc.size(); ++elem){
+        freqAcc[elem] = freqAcc[elem] + freqAcc[elem - 1];
     }
 
-    std::vector<type_t> result(maxElem);
+    for(auto elem : freqAcc)
+        std:: cout << elem << " ";
+    std::cout << "\n";
+
+    std::vector<type_t> result(input.size());
 
     for (int i = input.size() - 1; i >= 0 ; --i){
         result[freqAcc[input[i]] - 1] = input[i];
+        --freqAcc[input[i]];
     }
 
     return result;
 }
 
+int main(){
+    std::vector<int>nums {1,5,1,1,6,4};
+    std::vector<int>res = counting_sort(nums, 6);
+
+    for(auto elem : res)
+        std:: cout << elem << " ";
+    std::cout << "\n";
+}
