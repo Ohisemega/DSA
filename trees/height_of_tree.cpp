150413837d240b7a78c350b33983007efd387501
Ohisemega <famousalele@gmail.com>
2024-08-27 20:06:35 +0100

    Implement iterative max depth of tree

--- trees/height_of_tree.cpp
+++ trees/height_of_tree.cpp
@@ -1,11 +1,46 @@
 #include "tree.hpp"
+#include <queue>
 
 int max_tree_depth_recursive(Tree* root){
     if(root == nullptr){
         return  -1; // we return -1 because our depth algorithm assumes that every function call automatically
                     // adds depth level +1 to the tree's height. i.e. line 8
     }
-    int u = max_tree_depth_recursive(root->left) + 1;
-    int v = max_tree_depth_recursive(root->right) + 1;
-    return (u > v ? u : v);
+    int u = max_tree_depth_recursive(root->left);
+    int v = max_tree_depth_recursive(root->right);
+    return (u > v ? u + 1 : v + 1);
+}
+
+
+int max_tree_depth_iterative(Tree* node){
+    std::queue<Tree*> que;
+    Tree* current = node;
+    int height = -1;
+    if(node == nullptr) return height;
+    
+    que.emplace(node);
+    que.emplace(nullptr);
+
+
+    while(!que.empty()){
+        current = que.front();
+        que.pop();
+
+        if(current == nullptr){
+            ++height;
+
+            if(!que.empty()){
+                que.emplace(nullptr);
+            }            
+        }
+        
+        if(current->left){
+            que.emplace(current->left);
+        }
+        if(current->right){
+            que.emplace(current->right);
+        }
+    }
+
+    return height;
 }
