2e94350caf55175da9361f0fad126e1d2dec7d56
Ohisemega <famousalele@gmail.com>
2024-08-27 18:53:45 +0100

    Tree depth/height Alg.
    Add recursive algorithm to calculate the tree's height/depth.

--- trees/height_of_tree.cpp
+++ trees/height_of_tree.cpp
@@ -1,5 +1,11 @@
 #include "tree.hpp"
 
-int max_tree_depth(Tree* root){
-    
+int max_tree_depth_recursive(Tree* root){
+    if(root == nullptr){
+        return  -1; // we return -1 because our depth algorithm assumes that every function call automatically
+                    // adds depth level +1 to the tree's height. i.e. line 8
+    }
+    int u = max_tree_depth_recursive(root->left) + 1;
+    int v = max_tree_depth_recursive(root->right) + 1;
+    return (u > v ? u : v);
 }
