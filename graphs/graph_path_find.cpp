33507022469bddda8fca6ffc9f346f91785308d5
Ohisemega <famousalele@gmail.com>
2025-12-24 15:27:36 +0000

    Add BFS Unweighted Shortest Path Find

---
+++ graphs/graph_path_find.cpp
@@ -0,0 +1,19 @@
+/** Finding the Shortest path in a graph between two nodes: x (start) & y (end),
+ * can be achieved by using the BFS algorithm to build an array of the parent node
+ * IDs for every child ID (the array's index).
+ * But the BFS MUST be built using x's ID as the root node to work!
+ * Secondly, the graph MUST be UNWEIGHTED for this to work! 
+ */
+
+#include "graph.h"
+
+using NodeID = int;
+void find_path(NodeID start, NodeID end, NodeID parents[]) {
+    if(start == end || start == -1) {
+        std::cout << '\n' << start << '\n';
+    }else{
+        find_path(start, parents[end], parent);
+        std::cout << '\n' << parents[end] << '\n';
+    }
+
+}
